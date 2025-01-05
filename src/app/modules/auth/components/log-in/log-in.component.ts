6ada871875e695b873c557f9f2c25a9424bbd9ec
Fejin Francis <fejinfm2000@gmail.com>
2025-01-05 22:33:22 +0530

    Added login page

--- src/app/modules/auth/components/log-in/log-in.component.ts
+++ src/app/modules/auth/components/log-in/log-in.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { VisitorService } from '../../../web/service/visitor.service';
 
 @Component({
   selector: 'app-log-in',
   standalone: false,
-  
+
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private visitorService: VisitorService) {
+    this.loginForm = this.fb.group({
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required],
+    });
+  }
 
+  onLogin() {
+    console.log(this.loginForm);
+  }
 }
