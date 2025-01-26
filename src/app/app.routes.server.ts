8c548afbbe7d62f4f8c0082881c2c7cf5114fe2f
Fejin Francis <fejinfm2000@gmail.com>
2025-01-26 22:45:03 +0530

    prerendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: (RenderMode as any).SSR, // Dynamically render all routes
-  },
+    renderMode: RenderMode.Prerender
+  }
 ];
